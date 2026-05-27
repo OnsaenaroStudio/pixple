7edff89336c45b8e889bdb87882e0dbe9cc85a32
AS7AR <apo2073@outlook.com>
2026-05-27 09:00:18 +0900

    fix: rename package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pixple"
+    namespace = "io.github.onsaenarostudio.pixple"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
